4b93b72bc7992bbcec8c5dfd3b7fa8d8988fa683
[john gunnarsson] <[johnmessoa@gmail.com]>
2024-07-09 13:14:49 +0200

    developing a logo, working with style for loginpage

--- src/pages/loginpage/Loginpage.jsx
+++ src/pages/loginpage/Loginpage.jsx
@@ -1,33 +1,43 @@
-import './Loginpage.css';
+import "./Loginpage.css";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 const Loginpage = () => {
+  return (
+    <main className="loginpage">
+      <div className="loginpage-logo">
+        <Link to="">
+          <img src={logo} className="logo" alt="VitalVibe" />
+        </Link>
+        <h1 className="logo-h1">VitalVibe</h1>
+      </div>
+      
 
-    return (
-        <main className='loginpage'>
-            <div className='loginpage-logo'>
-                <h1>VitalVibe</h1>
-            </div>
-            
-            <form className='login-form'>
-                <input className='username' type="text" placeholder='Användarnamn'></input><br></br>
-                <input className='password' type="text" placeholder='Lösenord'></input><br></br>
-                <div className='new-user'>
-                    <div>
-                        <a href="">Registrera konto</a>
-                    </div>
-                    <div>
-                        <a href=''>Glömt<br></br> lösenord?</a>
-                    </div>
-                    
-                </div>
-                <div className='login-button'>
-                    <button type="submit">Logga in</button>
-                </div>
-                
-            </form>
-        </main>
-    )
-
-}
+      <form className="login-form">
+        <input
+          className="username"
+          type="text"
+          placeholder="Användarnamn"
+        ></input>
+        <br></br>
+        <input className="password" type="text" placeholder="Lösenord"></input>
+        <br></br>
+        <div className="new-user">
+          <div>
+            <a href="">Registrera konto</a>
+          </div>
+          <div>
+            <a href="">
+              Glömt<br></br> lösenord?
+            </a>
+          </div>
+        </div>
+        <div className="login-button">
+          <button type="submit">Logga in</button>
+        </div>
+      </form>
+    </main>
+  );
+};
 
 export default Loginpage;
