51b725e5017a1b1553b52cb6041bcbd02a1b0337
[john gunnarsson] <[johnmessoa@gmail.com]>
2024-07-13 00:52:05 +0200

    finished with the login method, added authprovider to the vitalvibeApp.jsx

--- src/VitalVibeApp.jsx
+++ src/VitalVibeApp.jsx
@@ -1,6 +1,7 @@
 // STUFF
 import './VitalVibeApp.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './components/login/Authentication';
 
 // PAGES
 import Loginpage from './pages/loginpage/Loginpage'
@@ -11,12 +12,14 @@ function VitalVibeApp() {
 
   return (
     <div className='vitalvibe'>
+      <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Loginpage />}></Route>
           <Route path="/training" element={<TrainingPage />}></Route>
         </Routes>
       </BrowserRouter>
+      </AuthProvider>
     </div>
   )
 }
--- src/pages/loginpage/Loginpage.jsx
+++ src/pages/loginpage/Loginpage.jsx
@@ -1,68 +1,216 @@
+// import "./Loginpage.css";
+// import { Link, useNavigate } from "react-router-dom";
+// import logo from "../../assets/logo.png";
+// import {useState, useContext} from "react"
+// import { Authentication } from "../../components/login/Authentication.jsx";
+// import axios from "axios";
+
+// const Loginpage = () => {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const navigate = useNavigate();
+
+//   const {
+//     state: { user },
+//     dispatch,
+//   } = useContext(Authentication);
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     if (!username || (!password && password != data)) {
+//       alert("Fyll i ditt användarnamn och lösenord");
+//       return;
+//     }
+
+//     try {
+//       const { data } = await axios.post(
+//         `${import.meta.env.VITE_API_URL}/auth/signin`,
+//         {
+//           username,
+//           password,
+//         },
+//         {
+//           withCredentials: true, //this will show our cookie
+//         }
+//       );
+
+//       dispatch({
+//         type: "LOGIN",
+//         payload: data,
+//       });
+
+//       window.localStorage.setItem("user", JSON.stringify(data));
+//       localStorage.setItem("loggedInUserId", data.id);
+//       console.log("User login: " + username);
+//       console.log("userId: " + data.id);
+
+//       //this will redirect the user to home page
+
+//       await navigate("/");
+//       window.location.reload();
+//     } catch (err) {
+//       console.log(
+//         "Lösenordet eller användarnamnet du har angivit är felaktigt"
+//       );
+//     }
+//   };
+
+//   return (
+//     <>
+//       <main className="loginpage">
+//         <div className="loginpage-logo">
+//           <Link to="">
+//             <img src={logo} className="logo" alt="VitalVibe" />
+//           </Link>
+//           <h1 className="logo-h1">VitalVibe</h1>
+//         </div>
+
+//         <form className="login-form" onSubmit={handleSubmit}>
+//           <input
+//             className="username"
+//             type="text"
+//             placeholder="Användarnamn"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//           ></input>
+//           <br></br>
+//           <input
+//             className="password"
+//             type="text"
+//             placeholder="Lösenord"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           ></input>
+
+//           <br></br>
+//           <div className="new-user">
+//             <div>
+//               <a href="">Registrera konto</a>
+//             </div>
+//             <div>
+//               <a href="">
+//                 Glömt<br></br> lösenord?
+//               </a>
+//             </div>
+//           </div>
+//           <div className="login-button">
+//             <button type="submit">Logga in</button>
+//           </div>
+//         </form>
+//       </main>
+//     </>
+//   );
+// };
+
+// export default Loginpage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import "./Loginpage.css";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.png";
-import { useContext, useState } from "react";
-import { Authentication } from "../../components/authentication/Authentication";
-
+import { useState, useContext } from "react";
+import { Authentication } from "../../components/login/Authentication.jsx";
+import axios from "axios";
 
 const Loginpage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    
-    const navigate = useNavigate
+  const {
+    state: { user },
+    dispatch,
+  } = useContext(Authentication);
 
-    const {
-      state: { user },
-      dispacth
-    }= useContext(Authentication)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-
-      if (!username || !password && password != (data)) {
-        alert("Fill in username and password please")
-        return
-      }
-
-
-      try {
-        const {data} = await axios.post
-      }
+    if (!username || !password) {
+      alert("Fyll i ditt användarnamn och lösenord");
+      return;
     }
 
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_API_URL}/auth/signin`,
+        { username, password },
+        { withCredentials: true }
+      );
 
+      dispatch({
+        type: "LOGIN",
+        payload: data,
+      });
 
+      window.localStorage.setItem("user", JSON.stringify(data));
+      localStorage.setItem("loggedInUserId", data.id);
+      console.log("User login: " + username);
+      console.log("userId: " + data.id);
 
-
+      // Redirect the user to home page
+      navigate("/");
+      window.location.reload();
+    } catch (err) {
+      console.log("Lösenordet eller användarnamnet du har angivit är felaktigt");
+    }
+  };
 
   return (
     <main className="loginpage">
       <div className="loginpage-logo">
-        <Link to="">
+        <Link to="/">
           <img src={logo} className="logo" alt="VitalVibe" />
         </Link>
         <h1 className="logo-h1">VitalVibe</h1>
       </div>
 
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <input
           className="username"
           type="text"
           placeholder="Användarnamn"
-        ></input>
-        <br></br>
-        <input className="password" type="text" placeholder="Lösenord"></input>
-        <br></br>
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          className="password"
+          type="password"
+          placeholder="Lösenord"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
         <div className="new-user">
           <div>
-            <a href="">Registrera konto</a>
+            <Link to="/register">Registrera konto</Link>
           </div>
           <div>
-            <a href="">
-              Glömt<br></br> lösenord?
-            </a>
+            <Link to="/forgot-password">
+              Glömt
+              <br />
+              lösenord?
+            </Link>
           </div>
         </div>
         <div className="login-button">
