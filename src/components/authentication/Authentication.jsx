ee330e43a82dce3c8552681b5aae5a9f1154a60f
[john gunnarsson] <[johnmessoa@gmail.com]>
2024-07-12 23:45:00 +0200

    creating my method and call for the login.

--- src/components/authentication/Authentication.jsx
+++ src/components/authentication/Authentication.jsx
@@ -59,3 +59,5 @@ const AuthProvider = ({Children}) => {
         </Authentication.Provider>
     )
 }  
+
+export {Authentication, AuthProvider}
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,23 @@
+import { createContext } from "react"
+
+
+const Login = createContext();
+
+
+const LoginProvider = ({ children, username, password }) => {
+    
+
+    // this is the login method, and i will call it with login("username, "password)
+    const login = async
+
+
+
+
+
+
+
+    
+  return (
+    
+  )
+}
