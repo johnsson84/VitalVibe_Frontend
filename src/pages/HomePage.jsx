7d3cfc6b9edef8e39cacd0225a64f9c7b301eb69
Johan Johnsson <johan@johnsson-net.se>
2024-07-29 23:12:52 +0200

    Routes and homepage
    Fixat routes för homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,30 @@
 // PAGES
+import { Routes, Route, Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import Sidebar from "../components/sidebar/Sidebar";
+import AddTraining from "../components/training/AddTraining";
 
 // CSS
-//import "./HomePage.css";
+import "./HomePage.css";
 
 const HomePage = () => {
-
-
-
-    return (
-        <main>
-
-            <div className="homeMain">
-                   <Header></Header>
-                    <Sidebar></Sidebar>
-            </div>
-            
-
-        </main>
-    )
-}
+  return (
+    <>
+      <div className="homeMain">
+        <div className="homeLeft">
+          <Sidebar></Sidebar>
+        </div>
+        <div className="homeRight">
+          <div className="homeHeader">
+            <Header></Header>
+          </div>
+          <div className="homeContent">
+            <Outlet></Outlet>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HomePage;
