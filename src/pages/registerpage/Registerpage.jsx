c9a6d16e7ebc5a4599b750240756f0ef5a2f018d
[john gunnarsson] <[johnmessoa@gmail.com]>
2024-07-30 20:44:59 +0200

    debugging my validation for password and age.

--- src/pages/registerpage/Registerpage.jsx
+++ src/pages/registerpage/Registerpage.jsx
@@ -39,23 +39,26 @@ const Registerpage = () => {
   //   }));
   // };
 
-
   // should be registerData after (e,) like this (e, registerData)
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e, registerData) => {
     e.preventDefault();
 
-    // if (registerData.password !== registerData.confirmpassword) {
-    //   alert("the given password did not match");
-    //   return;
-    // }
-    // if (
-    //   (!Number,
-    //   isInteger(Number(registerData.age)) || Number(registerData.age) <= 0)
-    // ) {
-    //   alert("Enter a valid age");
-    //   return;
-    // }
+    // CHECK PASSWORD MATCH
+    if (registerData.password !== registerData.confirmpassword) {
+      alert("the given password did not match");
+      return;
+    }
 
+    // VALIDATE THE AGE
+    if (
+      !Number.isInteger(Number(registerData.age)) ||
+      Number(registerData.age) <= 0
+    ) {
+      alert("you must enter a valid age");
+      return;
+    }
+
+    // SETTING UP THE REQUEST OPTIONS
     var options = {
       method: "POST",
       headers: {
@@ -64,7 +67,7 @@ const Registerpage = () => {
       credentials: "include",
       body: JSON.stringify(registerData),
     };
-
+    // SENDING A REQUEST TO THE BACKEND API
     try {
       const res = await fetch(
         `${import.meta.env.VITE_API_URL}/auth/signup`,
@@ -91,152 +94,86 @@ const Registerpage = () => {
         </div>
         <div className="form-container">
           <form className="form" onSubmit={handleSubmit}>
-            <div className="form-subcontainer-1">
-              <br />
-              <input
-                type="text"
-                className="register-username"
-                name="username"
-                value={registerData.username}
-                onChange={handleInputChange}
-                placeholder="Användarnamn"
-              />
-              <br />
-              <input
-                type="password"
-                className="register-password"
-                name="password"
-                value={registerData.password}
-                onChange={handleInputChange}
-                placeholder="Lösenord"
-              />
-              <br />
-              <input
-                type="password"
-                className="confirmpassword"
-                name="confirmpassword"
-                value={registerData.confirmpassword}
-                onChange={handleInputChange}
-                placeholder="Bekräfta lösenord"
-              />
-              <br />
-              <input
-                type="email"
-                id="personalinfo"
-                className="email"
-                name="email"
-                value={registerData.email}
-                onChange={handleInputChange}
-                placeholder="E-mail"
-              />
-              <br />
-              <input
-                type="text"
-                id="personalinfo"
-                className="firstName"
-                name="firstName"
-                value={registerData.firstname}
-                onChange={handleInputChange}
-                placeholder="Förnamn"
-              />
-              <br />
-              <input
-                type="text"
-                id="personalinfo"
-                className="lastName"
-                name="lastName"
-                value={registerData.lastname}
-                onChange={handleInputChange}
-                placeholder="Efternamn"
-              />
-              <br />
-              <input
-                type="number"
-                id="personalinfo"
-                className="age"
-                name="age"
-                value={registerData.age}
-                onChange={handleInputChange}
-                placeholder="Ålder"
-              />
-            </div>
+            <br />
+            <input
+              type="text"
+              className="register-username"
+              name="username"
+              value={registerData.username}
+              onChange={handleInputChange}
+              placeholder="Användarnamn"
+            />
+            <br />
+            <input
+              type="password"
+              className="register-password"
+              name="password"
+              value={registerData.password}
+              onChange={handleInputChange}
+              placeholder="Lösenord"
+            />
+            <br />
+            <input
+              type="password"
+              className="confirmpassword"
+              name="confirmpassword"
+              value={registerData.confirmpassword}
+              onChange={handleInputChange}
+              placeholder="Bekräfta lösenord"
+            />
+            <br />
+            <input
+              type="email"
+              id="personalinfo"
+              className="email"
+              name="email"
+              value={registerData.email}
+              onChange={handleInputChange}
+              placeholder="E-mail"
+            />
+            <br />
+            <input
+              type="text"
+              id="personalinfo"
+              className="firstName"
+              name="firstName"
+              value={registerData.firstname}
+              onChange={handleInputChange}
+              placeholder="Förnamn"
+            />
+            <br />
+            <input
+              type="text"
+              id="personalinfo"
+              className="lastName"
+              name="lastName"
+              value={registerData.lastname}
+              onChange={handleInputChange}
+              placeholder="Efternamn"
+            />
+            <br />
+            <input
+              type="number"
+              id="personalinfo"
+              className="age"
+              name="age"
+              value={registerData.age}
+              onChange={handleInputChange}
+              placeholder="Ålder"
+            />
+            <br />
+            <input
+              type="number"
+              id="personalinfo"
+              className="vo2max"
+              name="vo2max"
+              value={registerData.vo2max}
+              onChange={handleInputChange}
+              placeholder="Vo2max"
+            />
 
-            <div className="form-subcontainer-2">
-              {/* <input
-                type="text"
-                id="personalinfo"
-                className="profilephoto"
-                name="profilephoto"
-                value={registerData.profilephoto}
-                onChange={handleInputChange}
-                placeholder="Klistra in Profil bild"
-              /> */}
-              <br />
-              <input
-                type="number"
-                id="personalinfo"
-                className="vo2max"
-                name="vo2max"
-                value={registerData.vo2max}
-                onChange={handleInputChange}
-                placeholder="Vo2max"
-              />
-              <br />
-              {/* <input
-                type="text"
-                id="distance"
-                className="fivekm"
-                name="fivekm"
-                value={registerData.fivekm}
-                onChange={handleInputChange}
-                placeholder="Bästa 5km"
-              />
-              <br />
-              <input
-                type="text"
-                id="distance"
-                className="tenkm"
-                name="tenkm"
-                value={registerData.tenkm}
-                onChange={handleInputChange}
-                placeholder="Bästa 10km"
-              />
-              <br />
-              <input
-                type="text"
-                id="distance"
-                className="fifteenkm"
-                name="fifteenkm"
-                value={registerData.fifteenkm}
-                onChange={handleInputChange}
-                placeholder="Bästa 15km"
-              />
-              <br />
-              <input
-                type="text"
-                id="distance"
-                className="halfmarathon"
-                name="halfmarathon"
-                value={registerData.halfmarathon}
-                onChange={handleInputChange}
-                placeholder="Bästa 21km"
-              />
-              <br />
-              <input
-                type="text"
-                id="distance"
-                className="marathon"
-                name="marathon"
-                value={registerData.marathon}
-                onChange={handleInputChange}
-                placeholder="Bästa 42km"
-              /> */}
-            </div>
             <div className="register-btn-container">
-              <button
-                className="register-btn"
-                type="submit"
-              >
+              <button className="register-btn" type="submit">
                 Skapa Konto
               </button>
             </div>
