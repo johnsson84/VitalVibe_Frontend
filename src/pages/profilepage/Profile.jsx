5e6d16df4b496cfff675a92e87229b3f4ce816bd
Johan Johnsson <johan@johnsson-net.se>
2024-08-07 23:25:09 +0200

    best result in progress

--- src/pages/profilepage/Profile.jsx
+++ src/pages/profilepage/Profile.jsx
@@ -1,16 +1,43 @@
 import './Profile.css';
 import { UserContext } from '../../context/user/UserContext';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ThemeColorContext } from '../../context/themeColor/ThemeColorContext';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
 
     const { getLoggedInUserInfo, currentUserInfo } = useContext(UserContext);
     const { style } = useContext(ThemeColorContext);
+    const [ circlePosition, setCirclePosition ] = useState(1);
 
     const handleClick = () => {
         alert("test");
     }
+
+    const switchActivity = (circlePosition) => {
+        if (circlePosition === 1) {
+            return (
+                <div className='profileBestResultSwitchCircle1'></div>
+            )
+        }
+        if (circlePosition === 2) {
+            return (
+                <div className='profileBestResultSwitchCircle2'></div>
+            )
+        }
+        if (circlePosition === 3) {
+            return (
+                <div className='profileBestResultSwitchCircle3'></div>
+            )
+        } else {
+            return null
+        }
+    }
+    // useEffect(() => {
+    //     switchActivity();
+    // }, [circlePosition])
+
+    const activeCircleClass = `profileBestResultSwitchCircle${circlePosition}`;
 
     useEffect(() => {
         setTimeout(() => {
@@ -48,6 +75,20 @@ const Profile = () => {
                 <div className='profileUserInfoContent'>
                     <p>Vikt:</p>
                     {currentUserInfo.weight}
+                </div>
+            </div>
+
+            {/* ======================================== */}
+            {/** User best result section */}
+            <div className='profileBestResults'>
+                <div className='profileBestResultsIcons'>
+                    <button className='profileBestResultIconButton' onClick={() => setCirclePosition(1)}><img src='runner.svg'></img></button>
+                    <button className='profileBestResultIconButton' onClick={() => setCirclePosition(2)}><img src='biker.svg'></img></button>
+                    <button className='profileBestResultIconButton' onClick={() => setCirclePosition(3)}><img src='walker.svg'></img></button>
+                </div>
+                <div className='profileBestResultSwitch'>
+                    {/* {switchActivity(circlePosition)} */}
+                    <div className={activeCircleClass}></div>
                 </div>
             </div>
 
