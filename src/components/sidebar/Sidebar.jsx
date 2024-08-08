8616cc89c47a7ee52224e7bb6f3700b30068fdac
Johan Johnsson <johan@johnsson-net.se>
2024-08-08 22:06:35 +0200

    lagt till ikoner till sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
 
   const { style } = useContext(ThemeColorContext);
 
-    const [panelStatus, setPanelStatus] = useState(1);
+    const [panelStatus, setPanelStatus] = useState(0);
 
     const [ showModal, setShowModal ] = useState(false);
 
@@ -46,31 +46,38 @@ const Sidebar = () => {
   return (
     
       <div className="side-main">
-        <nav>
-          {/** Maybe we change text to icons later? */}
+        <nav style={style}>
 
-          <Link style={style} className={panelStatus === 1 ? "sideLink" : "sideLinkHide"} to="/profile">
-            Profil
+          <Link className="sideLink" to="/profile">
+            <img src="nav_profile.svg" />
+            <p className={panelStatus === 1 ? null : "sideLinkHide"}>Profile</p>
           </Link>
-          <Link style={style} className={panelStatus === 1 ? "sideLink" : "sideLinkHide"} to="/profile/training">
-            Logga träning
+          <Link className="sideLink" to="/profile/training">
+            <img src="nav_training.svg" />
+            <p className={panelStatus === 1 ? null : "sideLinkHide"}>Logga träning</p>
           </Link>
-          <Link style={style} className={panelStatus === 1 ? "sideLink" : "sideLinkHide"} to="/profile/food">
-            Logga mat
+          <Link className="sideLink" to="/profile/food">
+            <img src="nav_food.svg" />
+            <p className={panelStatus === 1 ? null : "sideLinkHide"}>Logga mat</p>
           </Link>
-          <Link style={style} className={panelStatus === 1 ? "sideLink" : "sideLinkHide"} to="/profile/challenges">
-            Utmaningar
+          <Link className="sideLink" to="/profile/challenges">
+            <img src="nav_challenges.svg" />
+            <p className={panelStatus === 1 ? null : "sideLinkHide"}>Utmaningar</p>
           </Link>   
-          <Link style={style} className={panelStatus === 1 ? "sideLink" : "sideLinkHide"} to="/profile/about">
-            Om oss
+          <Link className="sideLink" to="/profile/about">
+            <img src="nav_about.svg" />
+            <p className={panelStatus === 1 ? null : "sideLinkHide"}>Om oss</p>
           </Link>
-          <Link style={style} className={panelStatus === 1 ? "sideLink" : "sideLinkHide"} to="/profile/settings">
-            Inställningar
+          <Link className="sideLink" to="/profile/settings">
+            <img src="nav_settings.svg" />
+            <p className={panelStatus === 1 ? null : "sideLinkHide"}>Inställningar</p>
           </Link>
-          <Link style={style} className={panelStatus === 1 ? "sideLink" : "sideLinkHide"} onClick={handleShow}>
-            Logga ut
+          <Link className="sideLink" onClick={handleShow}>
+            <img src="nav_signout.svg" className={panelStatus === 1 ? "linkImg" : null} />
+            <p className={panelStatus === 1 ? null : "sideLinkHide"}>Logga ut</p>
           </Link>
           <LogoutModal isOpen={showModal} onClose={handleClose}></LogoutModal>
+          
         </nav>
         {showPanel()}
       </div>
