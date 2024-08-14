64a25b549dc7e93c94ee666c9c099e5cd6cf41ed
Johan Johnsson <johan@johnsson-net.se>
2024-08-14 15:58:44 +0200

    minor bug fix

--- src/pages/profilepage/Profile.jsx
+++ src/pages/profilepage/Profile.jsx
@@ -147,40 +147,7 @@ const Profile = () => {
     }
   };
 
-  let logoutTimer;
-  const checkLoginTime = () => {
-
-    // Get encoded date from localstorage and decode
-    const encodedDate = localStorage.getItem("loginDate");
-    const loginDate = new Date(window.atob(encodedDate));
-    // Check current date
-    const currentDate = new Date();
-    // Check time difference between login time and current time.
-    const diffTime = currentDate.getTime() - loginDate.getTime();
-    // Change last number for shorter logout interval for testing purpose ex 3 for three minutes.
-    const timeBeforeLogout = 1000 * 60 * 60; // Default 60.
-
-    // Comment out for testing in console.
-    // console.log(`Logout in: ${(diffTime / 1000) / 60} min / ${(timeBeforeLogout / 1000) / 60} min`);
-
-    // Check if difference time is equal or bigger than set logut time, default 60 min.
-    if (diffTime >= timeBeforeLogout) {
-      clearInterval(logoutTimer);
-      navigate("/logout");
-    }
-  };
-
-  useEffect(() => {
-    // Timer to check login time every minute.
-    logoutTimer = setInterval(() => {
-      checkLoginTime();
-    }, 1000 * 60);
-
-    return () => {
-      clearInterval(logoutTimer);
-    };
-  }, []);
-
+  
   useEffect(() => {
     setTimeout(() => {
       getLoggedInUserInfo();
