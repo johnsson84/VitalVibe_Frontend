6ead2685de43b2650a7f4272a54fec5f70fa5c9c
Johan Johnsson <johan@johnsson-net.se>
2024-08-14 17:53:59 +0200

    more bug fixes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,29 +10,36 @@ import { useNavigate } from "react-router-dom";
 import "./HomePage.css";
 import { useEffect } from "react";
 
-
-
 const HomePage = () => {
-
   const navigate = useNavigate();
   let logoutTimer;
   const checkLoginTime = () => {
-
     if (!localStorage.getItem("loginDate")) {
       navigate("/logout");
-    } 
+    }
     // Get encoded date from localstorage and decode
     const encodedDate = localStorage.getItem("loginDate");
-    const loginDate = new Date(window.atob(encodedDate));
+    let loginDate;
+    // Catch invalid characters after manipulation
+    try {
+      loginDate = new Date(window.atob(encodedDate));
+    } catch {
+      navigate("/logout");
+    }
+
     // Check current date
     const currentDate = new Date();
     // Check time difference between login time and current time.
     const diffTime = currentDate.getTime() - loginDate.getTime();
+    // Catch "Not A Number" error after manipulation.
+    if (isNaN(diffTime)) {
+      navigate("/logout");
+    }
     // Change last number for shorter logout interval for testing purpose ex 3 for three minutes.
     const timeBeforeLogout = 1000 * 60 * 60; // Default 60.
 
     // Comment out for testing in console.
-    // console.log(`Logout in: ${(diffTime / 1000) / 60} min / ${(timeBeforeLogout / 1000) / 60} min`);
+    // console.log(`Logout in: ${diffTime / 1000 / 60} min / ${timeBeforeLogout / 1000 / 60} min`);
 
     // Check if difference time is equal or bigger than set logut time, default 60 min.
     if (diffTime >= timeBeforeLogout) {
@@ -52,7 +59,6 @@ const HomePage = () => {
     };
   }, []);
 
-
   return (
     <>
       <div className="homeMain">
@@ -67,7 +73,7 @@ const HomePage = () => {
           <div className="homeContent">
             <Outlet></Outlet>
           </div>
-        {/* </div> */}
+          {/* </div> */}
         </div>
       </div>
     </>
